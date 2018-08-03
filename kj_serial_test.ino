f70be28303a65b744ecba4d444bbcba3b5a116df
Shohei Aoki <shoaok@gmail.com>
2018-08-03 11:20:38 +0900

    completed i/o control via serial port

--- kj_serial_test.ino
+++ kj_serial_test.ino
@@ -1,10 +1,23 @@
+#include <SoftwareSerial.h>
 #define STX 0x02
 #define ETX 0x03
 #define SPACE 0x20
+#define ENQ 0x05
+#define ACK 0x06
+#define EOT 0x04
+
+SoftwareSerial mySerial(6, 7); //232_TX,232_RX
 
 void setup() {
-	Serial3.begin(9600);
+	Serial.begin(19200);
+	// Serial3.begin(9600);
+	mySerial.begin(19200);
 	delay(1000);
+
+	// String base_command = "04DI  ";
+	// sendMessageBytes(base_command);
+	// String base_command = "0EPH  CH001P2000";
+	// sendMessageBytes(base_command);
 }
 
 void loop() {
@@ -13,17 +26,17 @@ void loop() {
 	//Channel: 0-399
 	//20.0[kPa]-800.0[kPa]
     //Set channel 1 to 400.0[kPa]
-
+	
 	for(int i=0;i<4;i++){
-		int pressure = (i+1)*100;
+		int pressure = (i+1)*1000;
 		String pressure_string = String(pressure);
 		String base_command = "0EPH  CH001P";
 		String p = base_command+pressure_string;
-		// String p = "4000";  		
-		// String p = "04DI  ";  //Test command
 		sendMessageBytes(p);
 		delay(1000);
 	}
+	
+	// delay(1000);
 }
 
 void sendMessageBytes(String command_and_data){
@@ -36,19 +49,35 @@ void sendMessageBytes(String command_and_data){
 		result = result - buf[i];
 	}
 	// byte result = 0x00 - 0x30 - 0x34 - 0x44 - 0x49 - 0x20 - 0x20; //-305->two's complement of 305->0b011001111->0xCF
-	byte ubyte = (result & 0b11110000) >> 4;
+	byte ubyte = ((result & 0b11110000) >> 4) & 0b11111111;
 	byte lbyte = result & 0b00001111;
 
+	char ubyteChar[2];
+	char lbyteChar[2];
+	sprintf(ubyteChar,"%x",ubyte);
+	sprintf(lbyteChar,"%x",lbyte);
+
+	mySerial.write(ENQ);
+	// Serial.println(buf);
 	//Here we send a message
 	//STX
-    Serial3.write(STX); 
+    mySerial.write(STX); 
+    // mySerial.write(STX); 
+    // Serial3.write(STX); 
 	//Payload
-    Serial3.write(buf,string_length); //do not send null terminator
+    mySerial.write(buf,string_length); //do not send null terminator
+    // mySerial.write(buf,string_length); //do not send null terminator
+    // Serial3.write(buf,string_length); //do not send null terminator
     //Checksum
-	Serial3.write(ubyte);
-	Serial3.write(lbyte);
+	mySerial.print(ubyteChar[0]);
+	mySerial.print(lbyteChar[0]);
+	// mySerial.write(ubyte);
+	// mySerial.write(lbyte);
+	// Serial3.write(ubyte);
+	// Serial3.write(lbyte);
 	//ETX
-    Serial3.write(ETX); 
-
+    mySerial.write(ETX); 
+    // mySerial.write(ETX); 
+    // Serial3.write(ETX); 
+	mySerial.write(EOT);
 }
-
